992b479537d68656637c0890cf1cbcc603136da9
alagusankar98 <66866351+alagusankar98@users.noreply.github.com>
2026-09-20 12:05:19 +0530

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.cpp
@@ -0,0 +1,40 @@
+struct DataMinVector{
+    std::vector<int> val;
+    std::vector<int> min;
+};
+
+class MinStack {
+private:
+    DataMinVector stackVar;
+public:
+    MinStack() {
+        stackVar.val.reserve(30000);
+        stackVar.min.reserve(30000);
+    }
+    
+    void push(int val) {
+        stackVar.val.push_back(val);
+        // Check if current val is less than already present min element
+        int minVal = (stackVar.min.empty()) ? val : std::min(val, stackVar.min.back());
+        stackVar.min.push_back(minVal);
+
+    }
+    
+    void pop() {
+        if(stackVar.val.empty()) return;
+        stackVar.val.pop_back();
+        stackVar.min.pop_back();
+    }
+    
+    int top() {
+        if(stackVar.val.empty()) return -1;
+
+        return stackVar.val.back();
+    }
+    
+    int getMin() {
+        if(stackVar.val.empty()) return -1;
+
+        return stackVar.min.back();
+    }
+};
